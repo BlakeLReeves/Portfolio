a7a62c8b09e12e0d0274e1bbb5435e757568f3e7
blakereeves19 <blake.reeves19@gmail.com>
2019-03-11 11:39:19 -0500

    Added styling to nav bar

--- src/client/shared/Navbar.tsx
+++ src/client/shared/Navbar.tsx
@@ -5,11 +5,27 @@ export interface NavbarProps { }
 
 const Navbar: React.SFC<NavbarProps> = () => {
     return (
-        <nav className="nav bg-dark fade-in">
-            <Link to="/" className="nav-link text-danger active">Home</Link>
-            <Link to="/contact" className="nav-link text-danger active">Contact</Link>
-            <Link to="/donate" className="nav-link text-danger active">Donate</Link>
-        </nav>
+        <ul className="nav nav-tabs tab-content bg-dark fade-in">
+            <li className="nav-item">
+                <Link to="/" className="nav-link text-info active">Home</Link>
+            </li>
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Separated link</a>
+                </div>
+            </li>
+            <li className="nav-item">
+                <Link to="/contact" className="nav-link text-info active">Contact</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/donate" className="nav-link text-info active">Donate</Link>
+            </li>
+        </ul>
     );
 }
 
